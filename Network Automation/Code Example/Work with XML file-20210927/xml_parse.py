ca382f0e2c6fefedd497eb8d7fa8290775ed27a5
murilodegrandi <88123863+murilodegrandi@users.noreply.github.com>
2021-09-27 18:46:25 +1000

    Delete Network Automation/Code Example directory

--- Network Automation/Code Example/Work with XML file-20210927/xml_parse.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-tree=ET.parse('country_data.xml')
-root=tree.getroot()
-print(root.tag)
-print(root.attrib)
-for child in root:
-    print(child.tag,child.attrib)
-    
