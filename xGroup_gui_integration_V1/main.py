c03fc5cdc1bd366b9158e47423da8f134aa57204
murilodegrandi <88123863+murilodegrandi@users.noreply.github.com>
2021-10-01 16:14:54 +1000

    Add files via upload
    Automation_function.py and main.py files will allow for individual device configuration. This is a test update... (only function conf_ospf function is done)

---
+++ xGroup_gui_integration_V1/main.py
@@ -0,0 +1,59 @@
+import datetime
+import yaml
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
+
+class functions:
+    def __init__(self,device,commands):
+        self.device = device
+        self.commands = commands
+
+    def send_show_command(self):# function to verify config
+        try:
+            with ConnectHandler(**self.device) as ssh:
+                ssh.enable()
+                for command in self.commands:
+                    output = ssh.send_command(command)
+                    print(output)
+                    t = datetime.datetime.now().strftime("%d.%m.%Y - %H;%M;%S")
+                    file = open("Show_command" + str(t) + ".txt", "w")
+                    file.write(output)
+                    file.close()
+            return output
+        except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+            print(error)
+
+    def send_config(self): # function to configure device
+        try:
+            with ConnectHandler(**self.device) as ssh:
+                ssh.enable()
+                output = ssh.send_config_set(self.commands)
+                print(output)
+                t = datetime.datetime.now().strftime("%d.%m.%Y - %H;%M;%S")
+                file = open("config_command" + str(t) + ".txt", "w")
+                file.write(output)
+                file.close()
+            return output
+        except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+            print(error)
+
+def inventory(): # function to load information from devices
+    try:
+        with open("devices.yaml") as f:
+            devices = f.read()
+        inventory_dict = yaml.load(devices)
+        return inventory_dict
+    except:
+        print("Error while loading inventory from file!")
+
+def config():
+    try:
+        with open("config.yaml") as f:
+            config = f.read()
+        config_list = yaml.load(config)
+        return config_list
+    except:
+        print("Error while loading configs from file!")
